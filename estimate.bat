095fa7bfbe7de4be00ad2d7c4d56c2ac67e4f6dd
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-04-04 21:04:48 +0200

    Update estimate.bat

--- estimate.bat
+++ estimate.bat
@@ -1,2 +1,2 @@
 rem estimate.bat afg
-call xsb64 --quietload --nobanner --noprompt -e "consult('xsb/wuenic_ver_3.pl'), consult('xsb/%1.pl'), estimate, halt." 2>warn30.txt
+call xsb64 --quietload --nobanner --noprompt -e "consult('xsb/wuenic_ver_3.pl'), consult('countries/%1.pl'), estimate, halt." 2>warn30.txt
